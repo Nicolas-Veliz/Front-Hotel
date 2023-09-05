eb92c09a310a672db39d3c980a4edb9995e3bf63
Nicolas-Veliz <nikolas13zzz3@gmail.com>
2023-09-05 19:51:59 -0300

    corrigiendo problema

--- src/Components/Footer/Footer.jsx
+++ src/Components/Footer/Footer.jsx
@@ -1,7 +1,7 @@
 import Container from 'react-bootstrap/Container';
 import Row from 'react-bootstrap/Row';
 import Col from 'react-bootstrap/Col';
-import './../PiedePagina/stylepiedepagina.css';
+import './../Footer/styleFooter.css';
 
 function PiedePagina() {
   return (
