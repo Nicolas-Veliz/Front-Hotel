5ccc77bd79311a65bfb269fbacaddeb480b4b992
Nicolas-Veliz <nikolas13zzz3@gmail.com>
2023-09-09 09:30:26 -0300

    agregando boton de inicio al navbar

--- src/Components/Navbar/Navbar.jsx
+++ src/Components/Navbar/Navbar.jsx
@@ -47,6 +47,7 @@ const Navegador = () => {
                         <Nav.Link className='action' href="#action8">Nosotros</Nav.Link>
                         <Nav.Link className='action' href="#action9">Contactos</Nav.Link>
                     </Nav>
+                    <button type="button" class="btn btn-outline-light">Iniciar Sesion/Registrarse</button>
                 </Navbar.Collapse>
             </Container>
         </Navbar>
