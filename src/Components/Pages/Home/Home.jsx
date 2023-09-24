b656f7d3c2b1ae1774cda449cf8f6582edff9d56
Nicolas-Veliz <nikolas13zzz3@gmail.com>
2023-09-23 23:14:17 -0300

    actualizando previaje

--- src/Components/Pages/Home/Home.jsx
+++ src/Components/Pages/Home/Home.jsx
@@ -1,6 +1,7 @@
 import Carousel from "../../Carousel/Carousel.jsx"
 import { Link } from "react-router-dom"
 import {Container} from "react-bootstrap";
+import '../../Pages/Home/styleHome.css'
 
 const Home = () => {
     return (
@@ -14,7 +15,7 @@ const Home = () => {
                         <Link href='https://previaje.gob.ar/'
                             target="_blank">
                             <img
-                                className="img-fluid d-block w-100"
+                                className="img-fluid"
                                 src="src/assets/img/Publicidad Previaje.jpg"
                                 alt="publicidad pre-viaje" 
                             />
