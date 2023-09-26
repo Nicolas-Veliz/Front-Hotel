418b5802bb320ab102aa455d35919c314355b725
Nicolas-Veliz <nikolas13zzz3@gmail.com>
2023-09-25 23:47:03 -0300

    creando catalogo de habitaciones

---
+++ src/Pages/Rooms Catalog/Rooms.jsx
@@ -0,0 +1,16 @@
+import Tarjetas from "../../Componentes/Tarjetas/Tarjetas"
+
+import './styleHabitaciones.css'
+
+
+const Rooms = () => {
+  return (
+    <>
+    <br/>
+    <h1>Habitaciones Disponibles</h1>
+        <Tarjetas />
+    </>
+  )
+}
+
+export default Rooms;
