311e5291abd087e475a94dcafb6ac8c3e8edcbd8
Nicolas-Veliz <nikolas13zzz3@gmail.com>
2023-11-15 11:34:34 -0300

    aplicando correciones

--- src/Routes/Routes.jsx
+++ src/Routes/Routes.jsx
@@ -1,11 +1,12 @@
-import {Route, Routes as Ruta} from 'react-router-dom'
-import Home from '../Pages/Home/Home.jsx'
-import Login from '../Components/Login/Login.jsx'
-import About from '../Pages/About/About.jsx';
-import Contact from '../Pages/Contact/Contact.jsx';
+import {Form, Route, Routes as Ruta} from 'react-router-dom';
+import Home from '../Pages/Home/Home.jsx';
+import Login from '../Components/Login/Login.jsx';
 import Record from '../Components/Record/Record.jsx';
+import Form from '../Components/Booking/Form.jsx'
+import About from '../Pages/About/About.jsx';
+import Contact from '../Pages/Contact/Contact.jsx'
 import Administrator from '../Components/Administrator/Administrator.jsx';
-import Inventory from '../Components/Administrator/Inventory/Inventory.jsx'
+import Inventory from '../Components/Administrator/Inventory/Inventory.jsx';
 
 const Routes = () => {
   return (
@@ -14,10 +15,11 @@ const Routes = () => {
       <Route path='/' element={<Home />} />
       <Route path='/Login' element= {<Login />}/>
       <Route path="/Record" element={<Record />} />
-      <Route path='/About' element={<About />}/>
-      <Route path='/Contact' element={<Contact />}/>
-      <Route path='/Administrator' element={<Administrator/>}/>
+      <Route path='/Form' element={<Form />}/>
       <Route path='/Inventory' element={< Inventory/>}/>
+      <Route path='/Administrator' element={<Administrator/>}/>
+      <Route path='/Contact' element={<Contact />}/>
+      <Route path='/About' element={<About />}/>
     </Ruta>
     </>
   )
