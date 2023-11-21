96192b247c1203a6c911defa99f2aabb5158ee7a
Nicolas-Veliz <nikolas13zzz3@gmail.com>
2023-11-20 21:26:16 -0300

    se corrigen parametros

--- src/Routes/Routes.jsx
+++ src/Routes/Routes.jsx
@@ -1,8 +1,8 @@
-import {Form, Route, Routes as Ruta} from 'react-router-dom';
+import { Route, Routes as Ruta} from 'react-router-dom';
 import Home from '../Pages/Home/Home.jsx';
 import Login from '../Components/Login/Login.jsx';
 import Record from '../Components/Record/Record.jsx';
-import Form from '../Components/Booking/Form.jsx'
+import Formm from '../Components/Booking/Formm.jsx'
 import About from '../Pages/About/About.jsx';
 import Contact from '../Pages/Contact/Contact.jsx'
 import Administrator from '../Components/Administrator/Administrator.jsx';
@@ -15,7 +15,7 @@ const Routes = () => {
       <Route path='/' element={<Home />} />
       <Route path='/Login' element= {<Login />}/>
       <Route path="/Record" element={<Record />} />
-      <Route path='/Form' element={<Form />}/>
+      <Route path='/Formm' element={<Formm />}/>
       <Route path='/Inventory' element={< Inventory/>}/>
       <Route path='/Administrator' element={<Administrator/>}/>
       <Route path='/Contact' element={<Contact />}/>
