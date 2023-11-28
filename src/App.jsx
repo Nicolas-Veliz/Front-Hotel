defa9647b6269f6cb4a018f3d21f9fe71ddca637
Nicolas-Veliz <nikolas13zzz3@gmail.com>
2023-11-27 23:50:47 -0300

    merge de footer

--- src/App.jsx
+++ src/App.jsx
@@ -1,3 +1,4 @@
+import Footer from './Components/Footer/Footer';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import Navbar from './Components/Navbar/Navbar';
 import Routes from './Routes/Routes.jsx';
@@ -9,12 +10,17 @@ import InventoryContext from './Context/InventoryContext'
 function App() {
   return (
     <>
+
       <UserContext>
         <InventoryContext>
           <Navbar />
           <Routes />
         </InventoryContext>
       </UserContext>
+      <div>
+        
+        <Footer />
+        </div>
     </>
   );
 }
